bae10d17f857095033af14ba74ee67620f9b043b
justYu2001 <jack4781012@gmail.com>
2023-04-03 15:06:37 +0800

    fix: invalid access token error handling
    Force users to sign out when the access token expires.

--- src/components/user/UserAvatar.tsx
+++ src/components/user/UserAvatar.tsx
@@ -1,27 +1,13 @@
 import Image from "next/image";
 
-import { AxiosError } from "axios";
-
-import SignOutDialog from "@/components/auth/SignOutDialog";
 import { useUser } from "@/hooks/user";
 import DefaultAvatar from "public/images/default-avatar.png";
 
 const UserAvatar = () => {
-    const { data: user, isLoading, isError, error } = useUser();
+    const { data: user, isLoading, isError } = useUser();
 
     if (isLoading || isError) {
-        const isLoggedOut = error instanceof AxiosError && error.response?.status === 401;
-
-        return (
-            <>
-                <SignOutDialog isOpen={isLoggedOut} />
-                <Image
-                    src={DefaultAvatar}
-                    alt=""
-                    className="w-11 rounded-full"
-                />
-            </>
-        );
+        return <Image src={DefaultAvatar} alt="" className="w-11 rounded-full" />;
     }
 
     return (
